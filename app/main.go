916c4c429cc6cc62495dcd5e6bc2af518ce32c4a
Ozan Uslan <github@ozanuslan.com>
2024-04-08 16:20:33 +0300

    feat: pass fourth stage

--- app/main.go
+++ app/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path"
+	"syscall"
 )
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
@@ -11,10 +13,43 @@ func main() {
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 
+	chroot := path.Join(os.TempDir(), fmt.Sprintf("%d", os.Getpid()))
+	chrootedCommand := path.Join(chroot, command)
+	err := os.MkdirAll(chroot, 0755)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "temp dir err: %v", err)
+		os.Exit(1)
+	}
+	commandFileData, err := os.ReadFile(command)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "command read err: %v", err)
+		os.Exit(1)
+	}
+	err = os.MkdirAll(path.Dir(chrootedCommand), 0755)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "command dir create err: %v", err)
+		os.Exit(1)
+	}
+	file, err := os.Create(chrootedCommand)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "command file create err: %v", err)
+		os.Exit(1)
+	}
+	os.Chmod(chrootedCommand, 0777)
+	_, err = file.Write(commandFileData)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "command write err: %v", err)
+		os.Exit(1)
+	}
+	file.Close()
+
 	cmd := exec.Command(command, args...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	err := cmd.Run()
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Chroot: chroot,
+	}
+	err = cmd.Run()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "err: %v", err)
 		os.Exit(cmd.ProcessState.ExitCode())
